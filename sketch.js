6367388ba0962295bf0dbafc54730038137e550c
camilyvick <vicamily08@gmail.com>
2024-05-24 12:17:51 -0300

    adicionando arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,44 @@
+let palavra;
+function setup() {
+  createCanvas(400, 400);   
+  let palavras=["Respeito","Obediencia","Educaçao"];
+  palavra= random (palavras);
+  
+
+  
+}
+ function inicializaCores (){
+  
+    background("white"); 
+  textSize(64)
+  fill("black")
+
+  textAlign(CENTER,CENTER)
+   
+   
+ }
+function draw() {
+  inicializaCores();
+  
+  
+  
+  
+let maximo=width;
+  let minimo=0;
+  let quantidade=map(mouseX,0,width,1,palavra.length);
+  let parcial= palavra.substring(0,quantidade);
+  text(parcial,200,200)
+  
+   //if(mouseX<50) {
+     //let palavra="R";
+   //  text(palavra,200,200);
+     
+   //}else{
+     //let palavra="respeito"
+    // text(palavra,200,200);
+     
+     
+     
+     
+   //}
+}
